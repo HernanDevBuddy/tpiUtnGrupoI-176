a7180f483b61aa84ec222ae21d2d70d0f48845a2
Carlos Garcia <corzuelachaco@gmail.com>
2023-04-11 16:28:40 -0300

    se actualizo y se agrego la validacionde los campos

--- codigo.js
+++ codigo.js
@@ -1,31 +1,98 @@
-window.addEventListener('load', ()=>{
-    let validadorFormlario = new FormValidator('formularioDatos',[{
-        name:'nombre',
-        display:'Nombre',
-        rules:'required|min_length[10]'
-    },
-    {
-        name:'apellido',
-        display:'Apellido',
-        rules:'required|min_length[10]'
-    },
-    {
-        name:'correo',
-        display:'Correo',
-        rules:'required|valid_email'
-    },
-    {
-        name:'linkEmpresa',
-        display:'LinkEmpresa',
-        rules:'valid_url'
-    }], function(errores, evento){
-        if(errores.length){
-            let mensaje='';
-
-            errores.forEach(function(campo, indice, arreglo){
-            mensaje+=`${campo.message}</br>`;
+window.addEventListener('load', () => {
+    let form = document.querySelector('#formularioDatos');
+    form.addEventListener('submit', (evento) => {
+        evento.preventDefault();
+        let validadorFormulario = new FormValidator(form, [
+            {
+                name:'nombre',
+                display:'Nombre',
+                rules:'required|alpha|min_length[5]'
+            },
+            {
+                name:'apellido',
+                display:'Apellido',
+                rules:'required|alpha|min_length[5]'
+            },
+            {
+                name:'numero',
+                display:'Numero',
+                rules:'required||numeric|exact_length[10]'
+            },
+            {
+                name:'correo',
+                display:'Correo',
+                rules:'required|valid_email'
+            },
+            {
+                name:'sitioweb',
+                display:'Sitio web',
+                rules:'valid_url'
+            }
+        ], (errores, evento) => {
+            if(errores.length){
+                let mensajes = '';
+                errores.forEach((campo, indice, arreglo) => {
+                    mensajes += `<div class="error-message ${campo.name}-error">${campo.message}</div>`;
+                });
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Error en el formulario',
+                    html: mensajes
+                });
+            } else {
+                Swal.fire({
+                    icon: 'question',
+                    title: '¿Estás seguro de enviar los datos?',
+                    showCancelButton: true,
+                    confirmButtonText: 'Sí, enviar',
+                    cancelButtonText: 'No, cancelar'
+                }).then((resultado) => {
+                    if (resultado.isConfirmed) {
+                        form.submit();
+                    }
+                });
+            }
         });
-            document.querySelector('#resultadoValidacion').innerHTML=mensaje;
-    }
+    });
 });
-});
+
+
+
+
+// window.addEventListener('load', ()=>{
+//     let validadorFormlario = new FormValidator('formularioDatos',[{
+//         name:'nombre',
+//         display:'Nombre',
+//         rules:'required|alpha|min_length[5]'
+//     },
+//     {
+//         name:'apellido',
+//         display:'Apellido',
+//         rules:'required|alpha|min_length[5]'
+//     },
+//     {
+//         name:'correo',
+//         display:'Correo',
+//         rules:'required|valid_email'
+//     },
+//     {
+//         name:'numero',
+//         display:'Numero',
+//         rules:'required|exact_length[10]|numeric'
+//     },
+//     {
+//         name:'sitioweb',
+//         display:'Sitio web',
+//         rules:'valid_url'
+//     }], function(errores, evento){
+//         if(errores.length){
+//             let mensaje='';
+
+//             errores.forEach(function(campo, indice, arreglo){
+//             mensaje+=`${campo.message}</br>`;
+//         });
+//             document.querySelector('#resultadoValidacion').innerHTML=mensaje;
+//     }
+// });
+// });
+
